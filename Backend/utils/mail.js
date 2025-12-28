96e4587dbe5b80bdeff57100bb1791d6c76ce612
rajesh ghosh <rajeshghosh96368@gmail.com>
2025-12-29 01:47:38 +0530

    Fix Otp

--- Backend/utils/mail.js
+++ Backend/utils/mail.js
@@ -1,7 +1,9 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // use SSL
     auth: {
         user: "rajeshghosh96368@gmail.com",   // your Gmail
         pass: "gkpe vebh wolw tgaj"      // NOT your Gmail password
